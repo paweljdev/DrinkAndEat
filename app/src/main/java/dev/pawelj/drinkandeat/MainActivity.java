dd7c56f69ceb6046da424c2da58db86b34319026
paweljdev <pawelj.dev@gmail.com>
2015-03-30 20:32:21 +0200

    Projekt GUI - woda

--- app/src/main/java/dev/pawelj/drinkandeat/MainActivity.java
+++ app/src/main/java/dev/pawelj/drinkandeat/MainActivity.java
@@ -100,4 +100,9 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void WaterGUI(View view) {
+        Intent intent = new Intent(view.getContext(), WaterActivity.class);
+        startActivityForResult(intent, 0);
+    }
 }
